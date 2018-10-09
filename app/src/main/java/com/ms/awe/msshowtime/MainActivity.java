26d275a27430367e2eedfdf9557e9ed7e2977caf
xubinjie <348147590@qq.com>
2018-10-09 16:40:00 +0800

    高仿微信

--- app/src/main/java/com/ms/awe/msshowtime/MainActivity.java
+++ app/src/main/java/com/ms/awe/msshowtime/MainActivity.java
@@ -9,17 +9,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ms.awe.msshowtime.activity.CopyActivity;
 import com.ms.awe.msshowtime.activity.ProgressBarActivity;
 import com.ms.awe.msshowtime.activity.WindowActivity;
+import com.ms.awe.msshowtime.activity.WxActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnToWindowAct;
     private Button btnToProgressAct;
     private Button btnToCopyAct;
+    private Button btnToWx;
     private TextView tvInfo;
 
     @Override
@@ -38,10 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnToWindowAct = findViewById(R.id.btn_window_act);
         btnToProgressAct = findViewById(R.id.btn_progress_bar);
         btnToCopyAct = findViewById(R.id.btn_copy_activity);
+        btnToWx = findViewById(R.id.btn_wx_activity);
 
         btnToWindowAct.setOnClickListener(this);
         btnToProgressAct.setOnClickListener(this);
         btnToCopyAct.setOnClickListener(this);
+        btnToWx.setOnClickListener(this);
 
         tvInfo = findViewById(R.id.tv_info);
     }
@@ -71,10 +74,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.btn_copy_activity:
-                Toast.makeText(this,"Start Copy Activity",Toast.LENGTH_SHORT).show();
                 intent = new Intent(MainActivity.this, CopyActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.btn_wx_activity:
+                intent = new Intent(MainActivity.this, WxActivity.class);
+                startActivity(intent);
             default:
                 break;
         }
---
+++ app/src/main/java/com/ms/awe/msshowtime/fragment/FriendMainTabFragment.java
@@ -0,0 +1,18 @@
+package com.ms.awe.msshowtime.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ms.awe.msshowtime.R;
+
+public class FriendMainTabFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_02, container, false);
+    }
+}
