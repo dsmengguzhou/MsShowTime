39b81bbade13d7610eb3b0293e4b3138561fcaf3
xubinjie <348147590@qq.com>
2019-04-04 16:54:08 +0800

    修改补充

--- app/src/main/java/com/ms/awe/msshowtime/ui/fragment/DiscoverFragment.java
+++ app/src/main/java/com/ms/awe/msshowtime/ui/fragment/DiscoverFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ms.awe.msshowtime.R;
 
@@ -15,23 +16,17 @@ import com.ms.awe.msshowtime.R;
 public class DiscoverFragment extends Fragment{
 
     private View rootView;
+    private TextView tvTitle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //避免重复创建
         if (rootView == null) {
             rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+            tvTitle = rootView.findViewById(R.id.tv_title);
+            tvTitle.setText(R.string.discover);
         }
         return rootView;
     }
 
-    public static HomeFragment newInstance(String extra) {
-        //利用Bundle传值
-        Bundle bundle = new Bundle();
-        bundle.putString("Home",extra);
-        //实例化
-        HomeFragment fragment = new HomeFragment();
-        fragment.setArguments(bundle);
-        return fragment;
-    }
 }
--- app/src/main/java/com/ms/awe/msshowtime/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/ms/awe/msshowtime/ui/fragment/HomeFragment.java
@@ -1,15 +1,13 @@
 package com.ms.awe.msshowtime.ui.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ms.awe.msshowtime.R;
-import com.ms.awe.msshowtime.utils.ToastUtil;
 
 /**
  * Created By Musi
@@ -18,22 +16,16 @@ import com.ms.awe.msshowtime.utils.ToastUtil;
 public class HomeFragment extends Fragment{
 
     private View rootView;
-    private RelativeLayout btnTab;
+    private TextView tvTitle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //避免重复创建
         if (rootView == null) {
             rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            btnTab = rootView.findViewById(R.id.btn_to_tab);
-            btnTab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ToastUtil.showShort(getContext(),"啥都没瞎点啥");
-                }
-            });
+            tvTitle = rootView.findViewById(R.id.tv_title);
+            tvTitle.setText(R.string.home);
         }
-
         return rootView;
     }
 
